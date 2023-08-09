123129725a72c2b17f6c31f91f9ff216f2c7c9dd
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-08-09 13:08:28 -0300

    listo

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ ps.theme()
 layout_1 = [
     [ps.Text("Mi primer menu",size=(40,10), auto_size_text=True)],
     [ps.Button("Dark Souls , Dark Souls 2, Dark Souls 3",key="BOTON", auto_size_button=True), 
-     ps.Button("O Bloodborne",key="BOTON2",auto_size_button=True)],
-     [ps.Output(size=(40,10))],
-     [ps.Image("BB_Dec_SS_7_1427134535.jpg")]
+    ps.Button("O Bloodborne",key="BOTON2",auto_size_button=True)],
+    [ps.Output(size=(40,10))],
+    [ps.Image(source='./mario (1).png')]
+    
+]
+
+layout_2 = [
+    [ps.Combo(['Opcion 1', 'Opcion2', 'Opcion 3'], key="Elige")]
 ]
 
 menu = ps.Window("Dark Souls 4Life",layout=layout_1)
+
 
 
 
@@ -21,6 +27,18 @@ while True:
     if eventos == ps.WIN_CLOSED:
         break
     if eventos == "BOTON":
+        menu.hide()
+        menu2 = ps.Window("Eleccion", layout=layout_2)
+        while True:
+            eventos_2, valores_2 = menu2.read()
+            if eventos_2 == ps.WIN_CLOSED:
+                menu.un_hide()
+                menu2.close()
+                break
+            if eventos_2 == "Elige":
+                print("elegiste")
         print("BOTON PRESIONADO")
     if eventos == "BOTON2":
         print("BOTON PRESIONADO 2")
+    if eventos == "BOTON2":
+        ps.popup("Creando un popup", title="BOTON2")
